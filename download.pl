c1cac5d0200e4e515b80e678253fef9bc239345f
unknown <mjbrown1123@gmail.com>
2015-02-22 15:49:03 -0600

    Image Download

---
+++ download.pl
@@ -0,0 +1,53 @@
+use warnings; 
+use strict;
+use LWP::Simple;
+use WWW::Mechanize;
+my $count = 0;
+
+my $url = "http://www.spanishdict.com";
+
+
+# gets the source code for the website and saves it to a file (close statement at the bottom of file)
+	#my $page = get($url);
+
+	#open(FILE, ">website.html");
+	#print FILE $page; 
+
+my $mech = WWW::Mechanize->new();
+$mech->get($url);
+
+my @links = $mech->find_all_images();
+
+for my $link (@links) {
+	my $urls = $link->url; 
+	my $length = length($urls);
+	my $next = substr($urls, $length - 3);
+
+
+	if (($next eq "png") || ($next eq "PNG")) {
+		if(!(substr($urls, 0, 7) eq "http://")) {
+			$urls = "http://www.spanishdict.com/" . $urls;
+		}
+		getstore($urls, 'image' . $count . '.png');
+		print "png saved successfully!\n";
+	}
+	elsif ($next eq "gif") {
+		if(!(substr($urls, 0, 7) eq "http://")) {
+			$urls = "http://www.spanishdict.com/" . $urls;
+		}
+		getstore($urls, 'image' . $count . '.gif');
+		print "gif saved successfully!\n";
+	}
+	elsif($next eq "jpg") {
+		if(!(substr($urls, 0, 7) eq "http://")) {
+			$urls = "http://www.spanishdict.com/" . $urls;
+		}
+		getstore($urls, 'image' . $count . '.jpg');
+		print "jpg saved successfully!\n";
+	}
+	
+	$count=$count+1;
+	
+}
+
+# close (FILE);
